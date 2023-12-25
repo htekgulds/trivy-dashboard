a1a34dd19f596fb899f995b1ca0340018ec07faf
Hasan TEKGÜL <hasan.tekgul@hmb.gov.tr>
2023-12-25 10:41:11 +0300

    ortak layout

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,19 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
-export default function App({ Component, pageProps }) {
+export default function App ({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <div>
+        <div className='navbar bg-base-300 shadow mb-4'>
+          <div className='container mx-auto'>
+            <div className='navbar-start'>
+              <a className='btn btn-ghost text-xl'>Trivy Dashboard</a>
+            </div>
+          </div>
+        </div>
+        <Component {...pageProps} />
+      </div>
     </QueryClientProvider>
   )
 }
